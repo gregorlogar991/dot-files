d11204bbd666dfbade6e925acfdf6f4ffd5126f0
Gregor Logar <gregorlogar991@gmail.com>
2023-04-12 11:37:22 +0200

    Fix export script, add new confs

--- export.sh
+++ export.sh
@@ -5,14 +5,16 @@ usage() { echo "Usage: $0 [git|export|all]" 1>&2; exit 1; }
 
 mv_files () {
    echo "Exporting profiles!"
-   cat /etc/profile > ~/dot-files/profile
-   cat /etc/starship.toml > ~/dot-files/starship.toml
-   cat ~/.bash_aliases > ~/dot-files/.bash_aliases
-   cat ~/.bash_functions > ~/dot-files/.bash_functions
+   cp /etc/profile ~/dot-files/profile
+   cp -r ~/.config/helix ~/dot-files/.config/
+   cp /etc/starship.toml ~/dot-files/starship.toml
+   cp ~/.bash_aliases ~/dot-files/.bash_aliases
+   cp ~/.bash_functions ~/dot-files/.bash_functions
+   cp -r ~/.warp/ ~/dot-files/.warp/
    sed -i -E 's/iam::[0-9]+:role/iam::XXXXXXXXXXXX:role/' ~/dot-files/.bash_functions
-   cat ~/.bashrc > ~/dot-files/.bashrc
-   cat ~/.vimrc > ~/dot-files/.vimrc
-   cat ~/.config/nvim/init.vim > ~/dot-files/init.vim
+   cp ~/.bashrc ~/dot-files/.bashrc
+   cp ~/.vimrc ~/dot-files/.vimrc
+   cp ~/.config/nvim/init.vim ~/dot-files/init.vim
    echo "Done!" ; echo "Brew bundle dump!"
    rm Brewfile
    brew bundle dump
